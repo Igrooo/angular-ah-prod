f84c759fcaca9e6e4bbeea1c32d93021d340a753
Igrooo <dev@ifaidy.infini.fr>
2019-09-17 14:32:09 +0200

    Remove wp

--- src/data/ext/wordpress/wp-content/themes/blankslate/nav-below-single.php
+++
@@ -1,5 +0,0 @@
-<?php $args = array(
-'prev_text' => '<span class="ah-ext-meta-nav">&larr;</span> %title',
-'next_text' => '%title <span class="ah-ext-meta-nav">&rarr;</span>'
-);
-the_post_navigation( $args ); ?>
